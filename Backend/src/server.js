fbd1813d3c660c42bde73943af0bc6fb488fb913
Raphelj <rapheljohnson.rap123@gmail.com>
2025-11-07 13:32:20 +0530

    ready for deployment

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
+import path from 'path';
 
 import notesRoutes from './routes/notesRoutes.js';
 import {connectDB} from './config/db.js';
@@ -8,17 +9,24 @@ import {connectDB} from './config/db.js';
 dotenv.config();
 
 const app = express();
+const __dirname = path.resolve();
 
 connectDB();
 
-app.use(cors({
-    origin: 'http://localhost:5173',
-}));
+if(process.env.NODE_ENV !== 'production') {
+    app.use(cors({
+        origin: 'http://localhost:5173',
+}))};
+
 //middleware to parse JSON
 app.use(express.json());
 
 
 app.use("/api/notes", notesRoutes);
+
+if (process.env.NODE_ENV === 'production'){
+    app.use(express.static(path.join(__dirname, "../Frontend/dist")));
+}
 
 app.listen(5001, () => {
     console.log('Server is running on port 5001');
